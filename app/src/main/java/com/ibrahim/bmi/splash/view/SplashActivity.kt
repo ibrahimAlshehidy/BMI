665aa9035422cf5bcbd2b0ffad053556b4d6698f
Ibrahim Alshehidy <ibrahim.alshehidy@gmail.com>
2020-01-27 16:59:38 +0200

    Creating project, adding mipmap files......

--- app/src/main/java/com/ibrahim/bmi/splash/view/SplashActivity.kt
+++ app/src/main/java/com/ibrahim/bmi/splash/view/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.ibrahim.bmi.splash.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ibrahim.bmi.R
+import kotlinx.android.synthetic.main.activity_add_details.*
 
 
 class SplashActivity : AppCompatActivity() {
@@ -16,10 +17,10 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun initUI() {
-//        val data =
-//            arrayOf("Berlin", "Moscow", "Tokyo", "Paris")
-//        npHeight.minValue = 0
-//        npHeight.maxValue = data.size - 1
-//        npHeight.displayedValues = data
+        val data =
+            arrayOf("Berlin", "Moscow", "Tokyo", "Paris")
+        npHeight.minValue = 0
+        npHeight.maxValue = data.size - 1
+        npHeight.displayedValues = data
     }
 }
